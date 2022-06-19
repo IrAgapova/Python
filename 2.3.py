990aa9e792f4737ece50e6c0604f9d683e1a844e
irina <www.rina01@gmail.com>
2022-06-19 15:35:27 +0400

    Задание к уроку 2

---
+++ 2.3.py
@@ -0,0 +1,10 @@
+n = int(input("Введите номер месяца:"))
+my_list = [1, 2, 12, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+if n in my_list[0:3]: print ("зима")
+if n in my_list[3:6]: print ("весна")
+if n in my_list[6:9]: print("лето")
+if n in my_list[9:13]: print ("осень")
+
+
+
+
