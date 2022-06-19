999e907586b6c6db2bce7373b29cae87bd41b40c
irina <www.rina01@gmail.com>
2022-06-19 07:03:44 +0400

    Python.change

--- 1.4.py
+++ 1.4.py
@@ -1,8 +1,11 @@
-n = input ("Введите число:")
-a = list(n)
+n = int (input ("Введите целое положительное число:"))
 max = 0
-i = 0
-while a[i] > max:
-    max = a[i]
-    i += 1
-print(max)
+num_n = n
+while num_n > 0:
+    digit = num_n % 10
+    if digit > max:
+        max = digit
+        if max == 9:
+            break
+        num_n // 10
+print("Наибольшая цифра в числе {n},равна {max}")
